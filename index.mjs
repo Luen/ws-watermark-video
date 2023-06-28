8ff20f15e27762bb8e5f16dc2f2e0efe47342f4f
L <Luen@users.noreply.github.com>
2023-06-28 13:36:19 +1000

    Path traversal fix.

--- index.mjs
+++ index.mjs
@@ -17,15 +17,15 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(cookieParser());
-app.use(csrf({ cookie: true })); // Implement CSRF protection
-app.use(helmet()); // Apply additional security headers
-app.use(compression()); // Compress all routes
-app.use(cors()); // Enable CORS for all routes
+app.use(csrf({ cookie: true }));
+app.use(helmet());
+app.use(compression());
+app.use(cors());
 const limiter = rateLimit({
-	windowMs: 15 * 60 * 1000,
-	max: 100,
-	standardHeaders: true,
-	legacyHeaders: false,
+    windowMs: 15 * 60 * 1000,
+    max: 100,
+    standardHeaders: true,
+    legacyHeaders: false,
 });
 app.use(limiter);
 
@@ -33,86 +33,87 @@ const logoPath = path.resolve(process.cwd(), 'Wanderstories-logo.png');
 const acceptedFormats = ['mp4'];
 const tempFileDir = path.resolve(process.cwd(), 'temp');
 const outputDir = path.resolve(process.cwd(), 'videos');
-// Check if the directory exists, if not create it
+const contentDir = path.resolve(process.cwd(), 'content');
+
+// Check if the directories exist, if not create them
 if (!fs.existsSync(outputDir)){
-	fs.mkdirSync(outputDir, { recursive: true });
+    fs.mkdirSync(outputDir, { recursive: true });
 }
 if (!fs.existsSync(tempFileDir)){
-	fs.mkdirSync(tempFileDir, { recursive: true });
-}	
+    fs.mkdirSync(tempFileDir, { recursive: true });
+}
 
 
 app.get('/', (req, res) => {
-	res.status(200).contentType('text/plain').send('Wanderstories Video Watermarker');
+    res.status(200).contentType('text/plain').send('Wanderstories Video Watermarker');
 });
 
 app.get('*', processVideoRequest);
 
 app.use((err, req, res, next) => {
-	console.error(err.stack);
-	res.status(500).send('Something went wrong!');
+    console.error(err.stack);
+    res.status(500).send('Something went wrong!');
 });
 
 app.listen(port, () => {
-	console.log(`Listening on port ${port}`);
+    console.log(`Listening on port ${port}`);
 });
 
 async function processVideoRequest(req, res, next) {
-	const url = path.normalize(req.path);
-    if (url.includes('../') || url.includes('..\\')) {
-        // Block the request if it tries to go to the parent directory
+    // Resolve the path to its absolute form to prevent path traversal
+    let requestedPath = path.resolve('/', req.path.slice(1));
+	// Remove the trailing slash if it exists
+	if (requestedPath.includes('../') || requestedPath.includes('..\\')) {
+        // Block the request if it contains path traversal
         return res.status(400).send('Invalid request');
     }
 
-    const originalVideoUrl = `https://wanderstories.space${url}`;
-    const fileExtension = path.extname(originalVideoUrl).slice(1).toLowerCase();
+    const fileExtension = path.extname(requestedPath).slice(1).toLowerCase();
     const isAcceptedFormat = acceptedFormats.includes(fileExtension);
 
-    if (!isAcceptedFormat || !isValidPath(url)) {
+    if (!isAcceptedFormat) {
         return res.status(404).send('Not Found');
     }
 
-	const filename = path.basename(originalVideoUrl);
-	const tempFilePath = path.join(tempFileDir, filename);
-	const outputPath = path.join(outputDir, filename);
+    const filename = path.basename(requestedPath);
+    const tempFilePath = path.join(tempFileDir, filename);
+    const outputPath = path.join(outputDir, filename);
 
-	try {
-		if (fs.existsSync(outputPath)) {
-			console.log('File already exists, returning cached version', filename);
-			return res.sendFile(outputPath);
-		}
-	
-		console.log('Processing video:', filename)
+    try {
+        const originalVideoUrl = `https://wanderstories.space${requestedPath}`;
 
-		const response = await axios({
-			method: 'GET',
-			url: originalVideoUrl,
-			responseType: 'stream',
-		});
-	
-		await new Promise((resolve, reject) => {
-			const writeStream = response.data.pipe(fs.createWriteStream(tempFilePath));
-			writeStream.on('finish', resolve);
-			writeStream.on('error', reject);
-		});
+        // Check for file existence and avoid reprocessing
+        if (fs.existsSync(outputPath)) {
+            console.log('File already exists, returning cached version', filename);
+            return res.sendFile(outputPath);
+        }
+        
+        console.log('Processing video:', filename)
 
-		await processVideo(tempFilePath, outputPath);
-	
-		return res.sendFile(outputPath);
-	
-	} catch (err) {
-		if (err.response && err.response.status === 404) {
-			console.error('Resource not found:', err.config.url);
-			return res.status(404).send('Not Found');
-		}
-		console.error('Unexpected error:', err);
-		next(err);
-   }
-}
+        const response = await axios({
+            method: 'GET',
+            url: originalVideoUrl,
+            responseType: 'stream',
+        });
+        
+        await new Promise((resolve, reject) => {
+            const writeStream = response.data.pipe(fs.createWriteStream(tempFilePath));
+            writeStream.on('finish', resolve);
+            writeStream.on('error', reject);
+        });
 
-function isValidPath(requestPath) {
-	const segments = requestPath.split('/');
-	return (segments[1] === 'content' && segments[2] === 'images' && segments[3] === 'videos') || segments[2] === 'media';
+        await processVideo(tempFilePath, outputPath);
+        
+        return res.sendFile(outputPath);
+
+    } catch (err) {
+        if (err.response && err.response.status === 404) {
+            console.error('Resource not found:', err.config.url);
+            return res.status(404).send('Not Found');
+        }
+        console.error('Unexpected error:', err);
+        next(err);
+    }
 }
 
 async function processVideo(inputPath, outputPath) {
